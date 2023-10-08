eb954f183d73cae4b9959be6557b15d17b73f912
Wesley <wmixgamer@gmail.com>
2023-10-08 17:20:43 -0300

    Ajuste no path das imagens

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,9 +10,9 @@ function Home() {
 
   const imagens = [
 
-    { id: '1', image: './src/assets/mexican1.jpg' },
-    { id: '2', image: './src/assets/mexican2.jpg' },
-    { id: '3', image: './src/assets/mexican3.jpg' },
+    { id: '1', image: './assets/mexican1.jpg' },
+    { id: '3', image: './assets/mexican3.jpg' },
+    { id: '2', image: './assets/mexican2.jpg' },
   ];
 
   useEffect(() => {
